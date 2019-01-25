71aabfd6999ac00538b8f1529db745eeca2e46ad
Wesley Hamilton <ffhighwind@gmail.com>
2019-01-24 22:25:28 -0700

    Added validation and IgnoreCase comparisons for Converters (may cause issues). OptionalAttribute is to be used for optional output properties.

---
+++ Converters/OptionalAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Converters
+{
+	[System.AttributeUsage(System.AttributeTargets.Property)]
+	public class OptionalAttribute : Attribute
+	{
+	}
+}
